9e37c7fc4962238cf20c9f63fd8d53ba56b411f1
Dorin Penea <dorin.penea@emag.ro>
2019-06-18 11:44:53 +0300

    Added setters + getters + doc blocks

--- src/DeliveryBundle/Entity/Delivery.php
+++ src/DeliveryBundle/Entity/Delivery.php
@@ -12,4 +12,22 @@ class Delivery
 
     /** @var Product[] */
     public $products;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Delivery
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
 }
