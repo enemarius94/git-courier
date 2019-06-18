cb57fd761b81cc86bb9041e067804a653cf48697
Ene Marius | eMAG <ene.marius@emag.ro>
2019-06-18 11:45:15 +0300

    Added getters and setters for Delivery entitiy and refactored a type-o

--- src/DeliveryBundle/Entity/Delivery.php
+++ src/DeliveryBundle/Entity/Delivery.php
@@ -6,10 +6,66 @@ use CommonBundle\Entity\Product\Product;
 
 class Delivery
 {
+    /** @var int $id */
     public $id;
 
+    /** @var string $address */
     public $address;
 
     /** @var Product[] */
     public $products;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Delivery
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     * @return Delivery
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return Product[]
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param Product[] $products
+     * @return Delivery
+     */
+    public function setProducts($products)
+    {
+        $this->products = $products;
+        return $this;
+    }
 }
