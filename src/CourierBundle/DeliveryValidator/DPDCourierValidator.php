491e9d66c48377df540e3ea1de9ee480a8e6ac59
ana.podeanu <ana.podeanu@emag.ro>
2019-06-18 12:02:06 +0300

    refactor product validation

--- src/CourierBundle/DeliveryValidator/DPDCourierValidator.php
+++ src/CourierBundle/DeliveryValidator/DPDCourierValidator.php
@@ -2,46 +2,32 @@
 
 namespace CourierBundle\DeliveryValidator;
 
-use CommonBundle\Entity\Product\Product;
 use CourierBundle\DeliveryValidator\Exception\DeliveryNotSupported;
 use DeliveryBundle\Entity\Delivery;
 
 class DPDCourierValidator
 {
-    const MAX_WEIGHT = 40000;
-    const MAX_PRODUCT_LENGTH = 2000;
-    const MAX_DELIVERY_VOLUME = 1000000000;
-
-    public function supportsDelivery(Delivery $delivery)
+    /**
+     * @param Delivery $delivery
+     * @param ProductValidator $productValidator
+     * @throws DeliveryNotSupported
+     */
+    public function supportsDelivery(Delivery $delivery, ProductValidator $productValidator)
     {
         $maxLength = 0;
         $maxWidth = 0;
         $maxHeight = 0;
 
         foreach ($delivery->products as $product) {
-            if ($product->type !== Product::DRY) {
-                throw new DeliveryNotSupported();
-            }
+            $productValidator->hasValidType($product);
+            $productValidator->hasValidWeight($product);
+            $productValidator->hasValidDimensions($product);
 
-            if ($product->weight > self::MAX_WEIGHT) {
-                throw new DeliveryNotSupported();
-            }
-
-            $dimmensions = [$product->length, $product->width, $product->height];
-            rsort($dimmensions);
-            list($length, $width, $height) = $dimmensions;
-            if ($length > self::MAX_PRODUCT_LENGTH) {
-                throw new DeliveryNotSupported();
-            }
-
-            $maxLength = max($maxLength, $length);
-            $maxWidth = max($maxWidth, $width);
-            $maxHeight += $height;
+            $maxLength = max($maxLength, $product->length);
+            $maxWidth = max($maxWidth, $product->width);
+            $maxHeight += $product->height;
         }
 
-        $totalVolume = $maxLength*$maxWidth*$maxHeight;
-        if ($totalVolume > self::MAX_DELIVERY_VOLUME) {
-            throw new DeliveryNotSupported();
-        }
+        $productValidator->hasValidVolume($maxLength, $maxWidth, $maxHeight);
     }
 }
